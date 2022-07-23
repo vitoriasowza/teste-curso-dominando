eb81336637435dcbf9850139e7452fc46c41719f
vitoriasowza <vitoriasowza.15@gmail.com>
2022-07-23 17:12:38 -0300

    primeiro programa eclipse

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package com.dio;
+
+import br.com.dio.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+	Gato gato = new Gato();
+	Livros Livros = new Livros();
+	
+	System.out.println(gato);
+	System.out.println(Livros);
+	
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World!" + (a+b));*/
+	}
+	
+}
+
+    class Livros {
+    	private String nome;
+    	private String npag;
+    }
